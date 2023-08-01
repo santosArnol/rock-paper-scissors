8f64446b11731637258f68b9cb040dd0f5c0118a
santosArnol <santosarnol@gmail.com>
2023-07-31 22:52:45 -0300

    Add playRound() function
    Take and compare player choice and PC choice, return a starting signifying the result

--- script.js
+++ script.js
@@ -2,13 +2,57 @@
 //Return choice as a string
 
 let getComputerChoice = () => {
-    const ranNum = Math.floor(Math.random()*3)+1;
+    const ranNum = Math.floor(Math.random() * 3) + 1;
     switch (ranNum) {
         case 1:
-            return "Rock"
+            return "rock"
         case 2:
-            return "Paper"
+            return "paper"
         case 3:
-            return "Scissors"                     
+            return "scissors"
     }
 }
+
+let playRound = (playerSelection, computerSelection) => {
+    console.log("PC choice was "+computerSelection)
+    if (playerSelection == "rock") {
+        switch (computerSelection) {
+            case "rock":
+                return "Draw";
+
+            case "paper":
+                return "You Lose!";
+
+            case "scissors":
+                return "You Win!";
+        }
+    }
+    else if (playerSelection == "paper") {
+        switch (computerSelection) {
+            case "paper":
+                return "Draw";
+
+            case "scissors":
+                return "You Lose!";
+
+            case "rock":
+                return "You Win!";
+        }
+    }
+    else {
+        switch (computerSelection) {
+            case "scissors":
+                return "Draw";
+
+            case "rock":
+                return "You Lose!";
+
+            case "paper":
+                return "You Win!";
+        }
+    }
+}
+
+let playerSelection = prompt("Make Your Choice: ")
+
+console.log(playRound(playerSelection,getComputerChoice()));
