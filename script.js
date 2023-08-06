b692737d110486e91f44bbde660c2b7a8fededc3
santosArnol <santosarnol@gmail.com>
2023-08-05 23:36:00 -0300

    Add round,score keeper,update and end functions

--- script.js
+++ script.js
@@ -16,10 +16,18 @@ const matchupResults = {
     }
 }
 
-let scoreKeeper = {
+let score = {
     player:0,
     pc:0,
 }
+
+const options = document.querySelectorAll('.option');
+
+const resultContainer = document.querySelector('.resultContainer');
+const resultChoicePlayer = document.querySelector('.resultChoicePlayer');
+const resultChoicePC = document.querySelector('.resultChoicePC');
+const resultExplain = document.querySelector('.resultExplain');
+const resultScore = document.querySelector('.resultScore');
 
 //Generate a random number (1-3), each stand for a respective choice
 //Return choice as a string
@@ -38,17 +46,69 @@ const getComputerChoice = () => {
 
 const getPlayerChoice = (e) => e.target.getAttribute("data-option");
 
+const keepScore = (result,score) =>{
+    if (result.includes("WIN")){
+        score.player++;
+    }
+    else if (result.includes("lose")){
+        score.pc++;       
+    }
+}
+
+const endGame = () => {
+    options.forEach(element => {
+        element.removeEventListener('click',playRound);        
+    });
+
+    const finalResult = document.createElement('p');
+    if (score.player == 5){
+        finalResult.textContent = "YOU WIN " + score.player + " - " +score.pc;;
+    }
+    else {
+        finalResult.textContent = "YOU LOSE " + score.player + " - " +score.pc;;
+    }
+
+    const resultContents = document.querySelectorAll('.resultContainer > *');
+    resultContents.forEach(element => {
+        element.remove();        
+    });
+        
+    resultContainer.append(finalResult);
+    
+
+}
+
+
+
 
 const playRound = (e) => {
+
+    const updateDisplay = () => {
+        resultChoicePlayer.textContent = "You chose "+ playerChoice;
+        resultChoicePC.textContent = "PC chose "+ computerChoice;    
+        resultExplain.textContent = matchupResults[playerChoice][computerChoice];
+        resultScore.textContent = score.player + " - " +score.pc;
+
+
+
+    }    
     
     const computerChoice = getComputerChoice();
     const playerChoice = getPlayerChoice(e);
     
-    console.log(matchupResults[playerChoice][computerChoice]);
+    result = matchupResults[playerChoice][computerChoice];
     
+    keepScore(result,score)
+
+    updateDisplay();
+    
+    if (score.pc == 5 || score.player == 5){
+        endGame();
+    }
+
 }
 
-const options = document.querySelectorAll('.option');
+
 
 options.forEach(element => {
     element.addEventListener('click',playRound);
